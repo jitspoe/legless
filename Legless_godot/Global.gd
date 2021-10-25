5ba42289ae05b9ab7aed176d4732090a75982f78
jitspoe <jitspoe@yahoo.com>
2021-10-25 01:20:26 -0400

    Bleh.  Seems I can't use motors, so this might be as far as I go with the ragdoll approach.

--- Legless_godot/Global.gd
+++ Legless_godot/Global.gd
@@ -1,3 +1,48 @@
 extends Node
 
+var ControllerDevice := 0
+var ControllerDeadzone := 0.8
 
+
+func joy_past_deadzone(joy_val : Vector2, axis_sign : float) -> bool:
+	var length := joy_val.length()
+	if (length > ControllerDeadzone):
+		var axis_positive := joy_val.x * axis_sign
+		var deadzone := (joy_val.normalized() * ControllerDeadzone).x  # Circular deadzone
+		var wedge_deadzone := 0.4142135623730950488016887242097 * abs(joy_val.y) # 1 / tan(3/4 90 degrees).  The slope of 1/8 of a circle
+		if (wedge_deadzone > deadzone):
+			deadzone = wedge_deadzone
+		if (axis_positive > deadzone):
+			return true
+	return false
+
+
+func is_pressed(action : String) -> bool:
+	var is_pressed := false
+	for event in InputMap.get_action_list(action):
+		if event is InputEventKey:
+			is_pressed = Input.is_key_pressed(event.scancode)
+		elif event is InputEventJoypadButton:
+			is_pressed = Input.is_joy_button_pressed(event.device, event.button_index)
+		elif event is InputEventMouseButton:
+			is_pressed = Input.is_mouse_button_pressed(event.button_index)
+		elif event is InputEventJoypadMotion:
+			var axis : int = event.axis
+			var axis_sign := sign(event.axis_value)
+			var axis_val := Input.get_joy_axis(ControllerDevice, event.axis)# * axis_sign
+			var perpendicular_axis_val := 0.0
+			if (axis == JOY_ANALOG_LX):
+				perpendicular_axis_val = Input.get_joy_axis(ControllerDevice, JOY_ANALOG_LY)
+			elif (axis == JOY_ANALOG_LY):
+				perpendicular_axis_val = Input.get_joy_axis(ControllerDevice, JOY_ANALOG_LX)
+			elif (axis == JOY_ANALOG_RX):
+				perpendicular_axis_val = Input.get_joy_axis(ControllerDevice, JOY_ANALOG_RY)
+			elif (axis == JOY_ANALOG_RY):
+				perpendicular_axis_val = Input.get_joy_axis(ControllerDevice, JOY_ANALOG_RX)
+			var value_2d := Vector2(axis_val, perpendicular_axis_val)
+			is_pressed = joy_past_deadzone(value_2d, axis_sign)
+		else:
+			continue
+		if is_pressed:
+			return true
+	return false
--- Legless_godot/MainScene.gd
+++ Legless_godot/MainScene.gd
@@ -1,15 +1,45 @@
 extends Spatial
 
 var RightHandTarget : Vector3
-onready var RightHandPhysicsBone : PhysicalBone = $"legless_man1/Armature/Skeleton/Physical Bone handR"
+onready var RightHandPhysicalBone : PhysicalBone = $"legless_man1/Armature/Skeleton/Physical Bone handR"
+onready var LeftHandPhysicalBone : PhysicalBone = $"legless_man1/Armature/Skeleton/Physical Bone handL"
+onready var RightLowerArmPhysicalBone : PhysicalBone = $"legless_man1/Armature/Skeleton/Physical Bone arm_lowerR"
+onready var LeftLowerArmPhysicalBone : PhysicalBone = $"legless_man1/Armature/Skeleton/Physical Bone arm_lowerL"
+const HAND_MOVE_IMPULSE := 1.0
+const HAND_TARGET_ACCEL := 10.0
+const MAX_TARGET_OFFSET := 5.0
+var LeftHandRelativeToCamera : Vector3
+const HAND_TARGET_SPEED := 5.0
+
 
 func _ready():
+	print(RightLowerArmPhysicalBone.get_property_list())
 	pass
 
 
-func _physics_process(_delta : float):
+func _physics_process(delta : float):
 	var MousePos := get_viewport().get_mouse_position()
 	var ViewportCam := get_viewport().get_camera()
-	var MousePosition3D := ViewportCam.project_position(MousePos, ViewportCam.global_transform.origin.z)
+	var CameraPos := ViewportCam.global_transform.origin
+	var MousePosition3D := ViewportCam.project_position(MousePos, CameraPos.z)
 	$DebugRightHand.global_transform.origin = MousePosition3D
-	
+	MoveHandTowardTarget(RightHandPhysicalBone, MousePosition3D)
+	if Global.is_pressed("move_left_target_left"):
+		LeftHandRelativeToCamera.x -= HAND_TARGET_SPEED * delta
+	if Global.is_pressed("move_left_target_right"):
+		LeftHandRelativeToCamera.x += HAND_TARGET_SPEED * delta
+	if Global.is_pressed("move_left_target_up"):
+		LeftHandRelativeToCamera.y += HAND_TARGET_SPEED * delta
+	if Global.is_pressed("move_left_target_down"):
+		LeftHandRelativeToCamera.y -= HAND_TARGET_SPEED * delta
+	var LeftHandTargetPos := CameraPos + LeftHandRelativeToCamera
+	LeftHandTargetPos.z = 0
+	$DebugLeftHand.global_transform.origin = LeftHandTargetPos
+	MoveHandTowardTarget(LeftHandPhysicalBone, LeftHandTargetPos)
+	#if Global.is_pressed("right_hand_grab"):
+	#	RightLowerArmPhysicalBone.set_param(
+
+
+func MoveHandTowardTarget(Hand : PhysicalBone, Target : Vector3):
+	var DirToTarget := Hand.global_transform.origin.direction_to(Target)
+	Hand.apply_central_impulse(DirToTarget * HAND_MOVE_IMPULSE)
